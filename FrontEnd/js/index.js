3fec5ef902de8c84009e0a80d82d7f3a5272a232
Ben Kerry <developerkerry@naver.com>
2020-08-11 11:56:02 +0900

    Register Form Created.

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -4,7 +4,14 @@ document.querySelector("#login").addEventListener("click", () => {
 
 document.querySelector("html").addEventListener("click", (e) => {
     if(e.target.className === "login_modal_layer"){
+        var form = document.querySelector(".login_form");
         document.querySelector("#login_modal").style.display = "none";
+        clearInputs(form);
+    }
+    else if(e.target.className === "register_modal_layer"){
+        var form = document.querySelector(".register_form");
+        document.querySelector("#register_modal").style.display = "none";
+        clearInputs(form);
     }
 });
 
@@ -13,5 +20,14 @@ document.querySelector("#login_btn").addEventListener("click", () => {
 });
 
 document.querySelector("#register_btn").addEventListener("click", () => {
-
+    document.querySelector("#login_modal").style.display = "none";
+    document.querySelector("#register_modal").style.display = "block";
 });
+
+function clearInputs(parentNode){
+    var inputElements = parentNode.querySelectorAll("input");
+
+    for(let i = 0; i < inputElements.length; i++){
+        inputElements[i].value = "";
+    }
+}
