8e98afd9e58aadadb0170458649860dcb0918605
Ben Kerry <developerkerry@naver.com>
2020-08-11 10:24:29 +0900

    Complete

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -2,8 +2,16 @@ document.querySelector("#login").addEventListener("click", () => {
     document.querySelector("#login_modal").style.display = "block";
 });
 
-document.querySelector("html").addEventListener("click", (e)=>{
+document.querySelector("html").addEventListener("click", (e) => {
     if(e.target.className === "login_modal_layer"){
         document.querySelector("#login_modal").style.display = "none";
     }
+});
+
+document.querySelector("#login_btn").addEventListener("click", () => {
+
+});
+
+document.querySelector("#register_btn").addEventListener("click", () => {
+
 });
